7d865495803a2e03fd6cefb5e86a04e28797a042
LukaKrickovic <krickovicluka@gmail.com>
2020-09-05 21:24:27 +0200

    Login & Registration validations added & vuejs implemented

--- static/login.js
+++ static/login.js
@@ -1,10 +1,70 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let passwordRegex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/);
+let usernameRegex = new RegExp(/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/);
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+var app = new Vue({
+	el: '#container',
+
+	data: {
+		username: "",
+		password: "",
+		role: "Guest",
+		roleRegistration: "Guest",
+		name: "",
+		surname: "",
+		email: "",
+		emailRegistration: "",
+		usernameRegistration: "",
+		passwordRegistration: "",
+		gender: "",
+		rightPanel: false
+	},
+
+	mounted() {
+
+	},
+
+	methods: {
+    	login : function() {
+			axios.
+				post('/login', this.username, this.password, this.role.toUpperCase())
+				.then(response => alert(this.username + this.password + this.role.toUpperCase()));
+		},
+
+		shiftPanels : function(){
+			this.rightPanel = !this.rightPanel;
+		},
+
+		attemptToRegister : function(){
+			/*if(!emailRegex.test(String(this.emailRegistration))){
+				alert("Email is invalid!");
+				return;
+			}*/
+
+			if(!usernameRegex.test(String(this.usernameRegistration))){
+				alert("Username is invalid!");
+				return;
+			}
+
+			if(!passwordRegex.test(String(this.passwordRegistration))){
+				alert("Password is invalid!");
+				return;
+			}
+
+			if(this.usernameRegistration === "" || this.roleRegistration === "" || this.name === "" || this.surname === "" || this.emailRegistration === ""){
+				alert("Please fill all fields!");
+				return;
+			}
+
+			this.register();
+		},
+
+		register : function(){
+			axios.
+				post('/register', this.username, this.password, this.role.toUpperCase())
+		}
+    },
+	filters: {
+
+	}
 });
